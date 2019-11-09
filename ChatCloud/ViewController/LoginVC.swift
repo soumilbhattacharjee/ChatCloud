79a06cdd128a959f5a4224bf79998a288faad10d
Soumil Bhattacharjee <soumil.dev@gmail.com>
2019-11-09 21:16:48 +0530

    Initial commit

---
+++ ChatCloud/ViewController/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  ChatCloud
+//
+//  Created by Soumil on 09/11/19.
+//  Copyright © 2019 Soumil. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "loginToDashboard", sender: self)
+    }
+    
+}
